e50b1d5b2c1f1110c594b42ed1f21e07f6ce09fc
mfritta <61205359+mfritta@users.noreply.github.com>
2024-04-23 09:33:47 +0200

    Add files via upload

---
+++ Anforderungsvergleich_v0.1.py
@@ -0,0 +1,21 @@
+from tkinter import filedialog
+import pandas as pd
+import matplotlib.pyplot as plt
+
+filename1 = filedialog.askopenfilename()
+df1 = pd.read_excel(filename1)
+filename2 = filedialog.askopenfilename()
+df2 = pd.read_excel(filename2)
+
+merged_df = pd.merge(df2, df1, on='Projektnummer', how='left', indicator=True)
+new_entries = merged_df[merged_df['_merge'] == 'left_only'][['Projektnummer', 'Projektbezeichnung_x']]
+new_entries.columns = ['Projektnummer', 'Projektbezeichnung']
+
+print("New Entries:")
+print(new_entries)
+
+output_excel_path = "new_entries.xlsx"
+new_entries.to_excel(output_excel_path, index=False)
+print(f"\nResult exported to {output_excel_path}")
+
+input("Press Enter to exit...")
